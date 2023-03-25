905ee6c3b1ce0c29af3fec9fd92fe45de70eaec3
GiangNguyen1207 <nguyengiang.nchg@gmail.com>
2023-03-25 13:13:57 +0200

    change server endpoint

--- client/src/services/OrderService.ts
+++ client/src/services/OrderService.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export async function getAllOrders() {
-  return await axios.get('http://localhost:8000/api/orders/get-all');
+  return await axios.get(
+    'https://northwind-server.onrender.com/api/orders/get-all'
+  );
 }
